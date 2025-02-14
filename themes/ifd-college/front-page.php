b14945955a523aaf0469ee6395aabf2e65ae2002
sforest-trident <scott@tridentmarketinguk.com>
2025-02-14 08:50:23 +0000

    Initial tweaks to front.page code

--- themes/ifd-college/front-page.php
+++ themes/ifd-college/front-page.php
@@ -7,10 +7,10 @@ $page_sub_title = get_post_meta(get_the_ID(), 'page_sub_title', true);
 ?>
 
 <div id="home-page-top">
-    <div style="background:url('<?php echo $featured_img_url; ?>') no-repeat scroll center right / contain; height: 140vh; margin-top: -80px; z-index: 9999; position: relative; margin-right: 11vw;"></div>
+    <div class="hero-feature-image" style="background:url('<?php echo $featured_img_url; ?>') no-repeat scroll center right / contain; height: 140vh; margin-top: -80px; z-index: 9999; position: relative; margin-right: 11vw;"></div>
 </div>
 
-<div id="page-top" style="margin-top: -150vh; width: 50vw;";>
+<div id="page-top">
     <div id="page-title" style="z-index: 9999;">
         <h1><?php echo !empty($page_title) ? $page_title : ''; ?></h1>
         <h2><?php echo !empty($page_sub_title) ? esc_html($page_sub_title) : ''; ?></h2>
