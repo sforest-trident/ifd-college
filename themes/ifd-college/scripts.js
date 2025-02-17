f8028c63f10831b24cf4405af4c63e7d6f43be2f
sforest-trident <scott@tridentmarketinguk.com>
2025-02-17 10:46:58 +0000

    Update rep with fresh remote pull

--- themes/ifd-college/scripts.js
+++ themes/ifd-college/scripts.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const header = document.querySelector(".header-widgets");
 
     window.addEventListener("scroll", function () {
-        if (window.scrollY > 50) { // Adjust threshold as needed
+        if (window.scrollY > 20) { // Adjust threshold as needed
             header.classList.add("scrolled");
         } else {
             header.classList.remove("scrolled");
