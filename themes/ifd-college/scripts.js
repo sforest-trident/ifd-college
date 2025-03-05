b6ed4b07b5c7271e9f5eea12dbb1ba9bc5e1947a
sforest-trident <scott@tridentmarketinguk.com>
2025-03-05 16:12:33 +0000

    Add addtional trigger point to sticky header scroll so that it triggers if the page loaded is not at the top (i.e refreshed)

--- themes/ifd-college/scripts.js
+++ themes/ifd-college/scripts.js
@@ -1,11 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
+(function(){
     const header = document.querySelector(".header-widgets");
+    const screenTogglePoint = 20;
+    const stickyHeaderClassName = 'scrolled';
 
-    window.addEventListener("scroll", function () {
-        if (window.scrollY > 20) { // Adjust threshold as needed
-            header.classList.add("scrolled");
-        } else {
-            header.classList.remove("scrolled");
+    // If top is offset on page load, make header sticky.
+    window.onload = function () {
+        if (this.window.scrollY > screenTogglePoint){
+            header.classList.add(stickyHeaderClassName);
         }
+    }
+
+    // Monitor scroll activity to toggle sticky header as necessary.
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > screenTogglePoint) { // Adjust threshold as needed
+            header.classList.add(stickyHeaderClassName);
+        } else {
+            header.classList.remove(stickyHeaderClassName);
+        }
+        
     });
-});
+})();
