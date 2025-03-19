6c2c55e23797205d037eef9a96e9131754120f66
sforest-trident <scott@tridentmarketinguk.com>
2025-03-19 13:51:57 +0000

    Modify sticky header toggler so that it's less responsive to scrollY, as it can be quite glitchy on mousescroll.

--- themes/ifd-college/scripts.js
+++ themes/ifd-college/scripts.js
@@ -3,6 +3,17 @@
     const screenTogglePoint = 20;
     const stickyHeaderClassName = 'scrolled';
 
+
+    // Function to toggle header styles
+    function toggleStickyHeader() {
+        if (window.scrollY > screenTogglePoint) { // Adjust threshold as needed
+            header.classList.add(stickyHeaderClassName);
+        } else {
+            header.classList.remove(stickyHeaderClassName);
+        }
+        return;
+    }
+
     // If top is offset on page load, make header sticky.
     window.onload = function () {
         if (this.window.scrollY > screenTogglePoint){
@@ -10,13 +21,10 @@
         }
     }
 
-    // Monitor scroll activity to toggle sticky header as necessary.
-    window.addEventListener("scroll", function () {
-        if (window.scrollY > screenTogglePoint) { // Adjust threshold as needed
-            header.classList.add(stickyHeaderClassName);
-        } else {
-            header.classList.remove(stickyHeaderClassName);
-        }
-        
+    // Create timeout handler to prevent fractional /subpixel rendering on scroll up, which is causing multiple calls to toggleStickyHeader in the one scroll.
+    let timeout;
+    window.addEventListener("scroll", () => {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => toggleStickyHeader(), 20); // This sets the delay to 20ms to allow the scrollY to settle before calling the function.
     });
 })();
