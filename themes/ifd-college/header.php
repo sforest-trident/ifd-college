29d4d4e63cb144a902c67e89f969c9ccfa3d4736
sforest-trident <scott@tridentmarketinguk.com>
2025-04-01 11:16:13 +0100

    Tweaks to header to add user and log in nav

--- themes/ifd-college/header.php
+++ themes/ifd-college/header.php
@@ -6,24 +6,35 @@
     
     <?php wp_head() ?>
 </head>
-<body class="<?php echo ('page_'.$post->post_name); ?>">
-<?php if (is_front_page()) { ?>
-    <header class="header-widgets">
-    <div class="home-header-widget-area">
-        <?php if (is_active_sidebar('header-home')) : ?>
-            <?php dynamic_sidebar('header-home'); ?>
+<body class="<?php echo (get_post_type($post).' '.$post->post_name); ?>">
+<header class="header-widgets">
+    <div class="<?php echo (is_front_page() ? 'home-header-widget-area' : 'header-widget-area'); ?>">
+        <?php if (is_front_page()) : ?>
+            <?php if (is_active_sidebar('header-home')) : ?>
+                <?php dynamic_sidebar('header-home'); ?>
+            <?php endif; ?>
+        <?php else: ?>
+            <?php if (is_active_sidebar('header-1')) : ?>
+                <?php dynamic_sidebar('header-1'); ?>
+            <?php endif; ?>
         <?php endif; ?>
+    
+
+        <?php 
+        // Show User logged in / logged out menu as appt:
+        if(!is_user_logged_in()){
+            wp_nav_menu(array(
+                'theme_location' => 'user-login'
+            ));
+        }else{
+            wp_nav_menu(array(
+                'theme_location' => 'user-menu'
+            ));
+            ?>
+        <?php
+        }
+        
+        ?>
     </div>
+    <button class="menu-toggle"><i class="nav-icon icon__hamburger"></i></button>
 </header>
-	
-<?php } else { ?>
-	
-	<header class="header-widgets">
-    <div class="header-widget-area">
-        <?php if (is_active_sidebar('header-1')) : ?>
-            <?php dynamic_sidebar('header-1'); ?>
-        <?php endif; ?>
-    </div>
-</header>
-	
-<?php } ?>
